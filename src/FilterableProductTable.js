bfba84927454e25e3aeb9d9f4e70ce0a407ec985
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-12-30 16:04:42 +0000

    thinking in react step 4 complete

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -3,11 +3,19 @@ import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
 
 class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+            inStockOnly: false
+        };
+    }
+
     render() {
         return (
             <div>
-                <SearchBar />
-                <ProductTable products={this.props.products} />
+                <SearchBar filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} />
+                <ProductTable products={this.props.products} filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} />
             </div>
         )
     }
